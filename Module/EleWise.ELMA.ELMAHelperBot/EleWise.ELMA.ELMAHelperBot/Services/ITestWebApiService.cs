f54417504e69a219508a093c19e05697bf064ad2
Данил Попов <d.popov@elewise.com>
2019-01-12 10:31:24 +0400

    Custom Auth Service

---
+++ Module/EleWise.ELMA.ELMAHelperBot/EleWise.ELMA.ELMAHelperBot/Services/ITestWebApiService.cs
@@ -0,0 +1,39 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using EleWise.ELMA.API.Models;
+using EleWise.ELMA.Services.Public;
+using EleWise.ELMA.Web.Service;
+
+namespace EleWise.ELMA.ELMAHelperBot.Services
+{
+    [ServiceContract(Namespace = APIRouteProvider.ApiServiceNamespaceRoot)]
+    [WsdlDocumentation(typeof(__ITestWebApiServiceResources), nameof(__ITestWebApiServiceResources.BotWebApiServiceDescription))]
+    [FaultContractBehavior(typeof(PublicServiceException))]
+    public interface IBotWebApiService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/CheckLogin?login={login}")]
+        [WsdlDocumentation(typeof(__ITestWebApiServiceResources), nameof(__ITestWebApiServiceResources.CheckLoginDescription))]
+        [return: WsdlParamOrReturnDocumentation(typeof(__ITestWebApiServiceResources), nameof(__ITestWebApiServiceResources.CheckLoginDescription))]
+        AuthResponse CheckLogin(
+            [WsdlParamOrReturnDocumentation(typeof(__ITestWebApiServiceResources), nameof(__ITestWebApiServiceResources.LoginDescription))] string login);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/BotAuth?something={something}&userName={userName}")]
+        [WsdlDocumentation(typeof(__ITestWebApiServiceResources), nameof(__ITestWebApiServiceResources.CheckLoginDescription))]
+        [return: WsdlParamOrReturnDocumentation(typeof(__ITestWebApiServiceResources), nameof(__ITestWebApiServiceResources.CheckLoginDescription))]
+        AuthResponse BotAuth(
+            [WsdlParamOrReturnDocumentation(typeof(__ITestWebApiServiceResources), nameof(__ITestWebApiServiceResources.LoginDescription))] string login,
+            [WsdlParamOrReturnDocumentation(typeof(__ITestWebApiServiceResources), nameof(__ITestWebApiServiceResources.UserNameDescription))] string userName,
+            [WsdlParamOrReturnDocumentation(typeof(__ITestWebApiServiceResources), nameof(__ITestWebApiServiceResources.PasswordDescription))] string password);
+    }
+
+    public class __ITestWebApiServiceResources
+    {
+        public static string BotWebApiServiceDescription { get { return SR.T("Сервис бота"); } }
+        public static string CheckLoginDescription { get { return SR.T("Проверка авторизации"); } }
+        public static string LoginDescription { get { return SR.T("Логин в боте"); } }
+        public static string UserNameDescription { get { return SR.T("Логин в ELMA"); } }
+        public static string PasswordDescription { get { return SR.T("Пароль"); } }
+    }
+}
